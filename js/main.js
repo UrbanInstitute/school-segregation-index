14145dd6f25db67392294cf967d53b6a67578140
Chartoff <bchartof@Informations-MacBook-Pro-2.local>
2020-02-04 13:05:40 -0500

    animation test

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const exploreDotPopScalar = .2;
 const uniformDotSize = 5;
-
+const M = 0.763666397;
 
 function getLevel(){
 	var levelButton = d3.select(".schoolLevelButton.active")
@@ -14,12 +14,12 @@ function updateDistrict(schools){
 	})
 	d3.selectAll("svg").remove()
 	d3.selectAll("table").remove()
-	
+
 	d3.select("#exploreDistrictName").text(schools[0]["gleaname"])
 	buildExploreScatter(schools)
-	buildExploreSciBar(schools)
-	buildExploreSchoolTypeSummary(schools)
-	buildSchoolList(schools)
+	// buildExploreSciBar(schools)
+	// buildExploreSchoolTypeSummary(schools)
+	// buildSchoolList(schools)
 }
 
 
@@ -255,6 +255,23 @@ function buildExploreScatter(data){
 
 	      
 	  // Add the scatterplot
+
+
+	  svg.selectAll("lollipop")
+	      .data(data)
+	    .enter().append("line")
+	      // .attr("r", function(d){ return exploreDotPopScalar*Math.sqrt(d.population)})
+	      .attr("x1", function(d) { return x(d.minority); })
+	      .attr("x2", function(d) { return x(d.minority); })
+	      .attr("y1", function(d) { return y(0); })
+	      .attr("y2", function(d) { return y(d.sci); })
+	      .classed("lollipop",true)
+	      .classed("explore",true)
+	      // .classed("charter", function(d){ return d.charter })
+	      // .classed("magnet", function(d){ return d.magnet })
+	      // .classed("private", function(d){ return d.private })
+	      // .classed("tps", function(d){ return d.tps })
+
 	  svg.selectAll("dot")
 	      .data(data)
 	    .enter().append("circle")
@@ -262,11 +279,16 @@ function buildExploreScatter(data){
 	      .attr("cx", function(d) { return x(d.minority); })
 	      .attr("cy", function(d) { return y(d.sci); })
 	      .classed("dot",true)
-	      .classed("explore",true)
-	      .classed("charter", function(d){ return d.charter })
-	      .classed("magnet", function(d){ return d.magnet })
-	      .classed("private", function(d){ return d.private })
-	      .classed("tps", function(d){ return d.tps })
+	      .style("fill", function(d,i){
+	      	if(d.minority < M) return "#55b748"
+	      	else return "#1696d2"
+	      })
+	      // .classed("explore",true)
+	      // .classed("charter", function(d){ return d.charter })
+	      // .classed("magnet", function(d){ return d.magnet })
+	      // .classed("private", function(d){ return d.private })
+	      // .classed("tps", function(d){ return d.tps })
+
 
 	  // Add the X Axis
 	  svg.append("g")
@@ -275,6 +297,7 @@ function buildExploreScatter(data){
 
 	  // Add the Y Axis
 	  svg.append("g")
+	  	.attr("class","y axis")
 	      .call(d3.axisLeft(y));
 
 
@@ -414,6 +437,252 @@ d3.selectAll(".schoolLevelButton")
 			toggleSchoolType(active, schoolType)	
 			
 		})
+
+
+
+
+
+
+
+
+
+	d3.select("#gogogo").on("click",function(){
+
+		var margin = {top: 20, right: 20, bottom: 30, left: 50},
+			    width = 600 - margin.left - margin.right,
+			    height = 500 - margin.top - margin.bottom;
+
+			// parse the date / time
+
+			// set the ranges
+			var y = d3.scaleLinear().range([height, 0]);
+
+
+			// Get the data
+
+			  // format the data
+			  // data.forEach(function(d) {
+			  //     d.sci = +d.SCI_sys;
+			  //     d.minority = +d.minority_school/+d.population_school;
+			  //     d.population = +d.population_school;
+			  //     d.charter = d.charter == "Yes";
+			  //     d.magnet = d.magnet == "Yes";
+			  //     d.private = d.private == "1";
+			  //     d.tps = d.tps == "1";
+			  // });
+
+			  // Scale the range of the data
+			  y.domain([0, 1]);
+
+			      
+			  // Add the scatterplot
+
+			  d3.selectAll(".dot")
+			  	.transition()
+			  	.duration(300)
+			  	.style("opacity",0)
+
+
+			  d3.select(".y.axis")
+			  	// .transition()
+			  	// .delay(300)
+			  	// .duration(1000)
+			  	.call(d3.axisLeft(y))
+
+			  var totalY = 0;
+			  var prevY = false;
+
+  			  var totalY2 = 0;
+			  var prevY2 = false;
+
+			  var totalYL = 0;
+			  var prevYL = false;
+
+  			  var totalY2L = 0;
+			  var prevY2L = false;
+
+  			  var totalYR = 0;
+			  var prevYR = false;
+
+  			  var totalY2R = 0;
+			  var prevY2R = false;
+
+			  var returnVal, returnVal2, returnValL, returnVal2L, returnValR, returnVal2R;
+
+			  d3.selectAll(".lollipop")
+			  	  .transition()
+			  	  .duration(700)
+			  	  .style("stroke", function(d,i){
+			  	  	if(d.minority < M){
+			  	  		return "#55b748"
+			  	  	}else{
+			  	  		return "#1696d2"
+			  	  	}
+			  	  })
+			      .transition()
+			      .duration(1000)
+			      .attr("y2", function(d) { return y(d.sci); })
+			      .transition()
+			      .delay(function(d,i){ return (171 - i) * 3})
+			      .duration(1400)
+			      
+			      .attr("y1", function(d,i){
+			      	if(prevY == false){
+			      		prevY = true;
+			      		totalY = d.sci
+			      		return y(0);
+			      	}else{
+			      		prevY = d.sci
+			      		totalY += d.sci
+			      		returnVal = totalY
+			      		// totalY += prevY
+			      		return y(returnVal)
+			      	}
+			      	// totalY += 
+			      	// return totalY
+
+			      	// console.log(i)
+
+			      })
+			      .attr("y2", function(d,i){
+			      	// console.log(d.sci)
+			      	if(prevY2 == false){
+			      		console.log(i,d)
+			      		prevY2 = true;
+			      		totalY2 = d.sci;
+			      		return y(d.sci);
+			      	}else{
+			      		prevY2 = d.sci
+			      		returnVal2 = totalY2
+			      		totalY2 += prevY2
+			      		return y(returnVal2)
+			      	}
+			      })
+  			      .attr("x1", 200)
+  			      .attr("x2", 200)
+  			      .style("stroke-width", "3px")
+  			      .transition()
+  			      .delay(function(d,i){ return 800 - (171 - i)*3})
+					.style("stroke-width", "60px")	
+			      .attr("x2", function(d,i){
+		      	if(d.minority < M){
+			      		return 200
+			      	}else{
+			      		return 400
+			      	}
+			      })
+			      .attr("x1", function(d,i){
+			      	if(d.minority < M){
+			      		return 200
+			      	}else{
+			      		return 400
+			      	}
+			      })
+			      .attr("x2", function(d,i){
+		      	if(d.minority < M){
+			      		return 200
+			      	}else{
+			      		return 400
+			      	}
+			      })
+			      .transition()
+			      .delay(function(d,i){ return i})
+			      .attr("y1", function(d,i){
+			      	if(d.minority < M){
+				      	if(prevYL == false){
+				      		prevYL = true;
+				      		totalYL = d.sci
+				      		return y(0);
+				      	}else{
+				      		prevYL = d.sci
+				      		totalYL += d.sci
+				      		returnValL = totalYL
+				      		// totalY += prevY
+				      		return y(returnValL)
+				      	}
+				     }else{
+				      	if(prevYR == false){
+				      		prevYR = true;
+				      		totalYR = d.sci
+				      		return y(0);
+				      	}else{
+				      		prevYR = d.sci
+				      		totalYR += d.sci
+				      		returnValR = totalYR
+				      		// totalY += prevY
+				      		return y(returnValR)
+				      	}
+
+				     }
+			      	// totalY += 
+			      	// return totalY
+
+			      	// console.log(i)
+
+			      })
+			      .attr("y2", function(d,i){
+			      	if(d.minority < M){
+				      	if(prevY2L == false){
+				      		console.log(i,d)
+				      		prevY2L = true;
+				      		totalY2L = d.sci;
+				      		return y(d.sci);
+				      	}else{
+				      		prevY2L = d.sci
+				      		returnVal2L = totalY2L
+				      		totalY2L += prevY2L
+				      		return y(returnVal2L)
+				      	}
+				     }else{
+				      	if(prevY2R == false){
+				      		console.log(i,d)
+				      		prevY2R = true;
+				      		totalY2R = d.sci;
+				      		return y(d.sci);
+				      	}else{
+				      		prevY2R = d.sci
+				      		returnVal2R = totalY2R
+				      		totalY2R += prevY2R
+				      		return y(returnVal2R)
+				      	}
+
+				     }
+			      })
+			      // .attr("x1",10)
+			      // .attr("x2",10)
+			      // .classed("charter", function(d){ return d.charter })
+			      // .classed("magnet", function(d){ return d.magnet })
+			      // .classed("private", function(d){ return d.private })
+			      // .classed("tps", function(d){ return d.tps })
+
+
+function getRandomColor() {
+  // var letters = '0123456789ABCDEF';
+  // var color = '#';
+  // for (var i = 0; i < 6; i++) {
+  //   color += letters[Math.floor(Math.random() * 16)];
+  // }
+  return "red";
+}
+
+
+			  // // Add the X Axis
+			  // svg.append("g")
+			  //     .attr("transform", "translate(0," + height + ")")
+			  //     .call(d3.axisBottom(x));
+
+			  // // Add the Y Axis
+			  // svg.append("g")
+			  //     .call(d3.axisLeft(y));
+
+
+
+	})
+
+
+
+
+
 }
 
 init()
