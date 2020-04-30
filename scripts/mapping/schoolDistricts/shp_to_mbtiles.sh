2532fa538cf152b36e52ecc50ba0c49b8e6b6994
Feng <afeng@urban.org>
2020-04-30 16:46:04 -0400

    Fix file paths in shp_to_mbtiles.sh to make the script run from root directory

--- scripts/mapping/schoolDistricts/shp_to_mbtiles.sh
+++ scripts/mapping/schoolDistricts/shp_to_mbtiles.sh
@@ -1,14 +1,14 @@
 
 # Reproject source shapefile (from NCES) to web mercator
 # Source: https://nces.ed.gov/programs/edge/Geographic/DistrictBoundaries, 2018 boundaries (2017-18 school year)
-ogr2ogr data/mapping/schoolDistricts/shapefile/schoolDistricts.shp -t_srs "EPSG:4326" schoolDistricts/source/EDGE_SCHOOLDISTRICT_TL18_SY1718/schooldistrict_sy1718_tl18.shp
+ogr2ogr data/mapping/schoolDistricts/shapefile/schoolDistricts.shp -t_srs "EPSG:4326" data/mapping/schoolDistricts/source/EDGE_SCHOOLDISTRICT_TL18_SY1718/schooldistrict_sy1718_tl18.shp
 
 #Convert shapefile to geojson
-ogr2ogr -f GeoJSON data/mapping/schoolDistricts/geoJson/schoolDistricts_tmp.geojson schoolDistricts/shapefile/schoolDistricts.shp
+ogr2ogr -f GeoJSON data/mapping/schoolDistricts/geoJson/schoolDistricts_tmp.geojson data/mapping/schoolDistricts/shapefile/schoolDistricts.shp
 
 
 python scripts/mapping/schoolDistricts/generateids.py k12-segregation
 
 
 #Convert geojson to mbtiles, min zoom level 6 (max 14 by default). Replace old mbtiles
-tippecanoe -o data/mapping/schoolDistricts/mbtiles/schoolDistricts.mbtiles schoolDistricts/geoJson/schoolDistricts.geojson -Z6 --force
+tippecanoe -o data/mapping/schoolDistricts/mbtiles/schoolDistricts.mbtiles data/mapping/schoolDistricts/geoJson/schoolDistricts.geojson -Z6 --force
