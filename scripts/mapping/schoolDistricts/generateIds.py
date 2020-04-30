03c2adb4b332e6dd2549ebac986d1d64204eb1cf
Chartoff <bchartof@Informations-MacBook-Pro-2.local>
2020-04-30 15:58:09 -0400

    python script for ids in json

---
+++ scripts/mapping/schoolDistricts/generateIds.py
@@ -0,0 +1,28 @@
+import json
+
+import csv
+import json
+from os import environ as env
+from sys import argv as argv
+
+
+with open("%s/%s/data/mapping/schoolDistricts/geoJson/schoolDistricts_tmp.geojson"%(env["PROJECT_PATH"], argv[1]),"r") as districtFileIn:
+    data = json.load(districtFileIn)
+
+features = data["features"]
+
+print(len(features))
+
+for f in features:
+    f["id"] = f["properties"]["GEOID"]
+
+data["features"] = features
+
+
+with open("%s/%s/data/mapping/schoolDistricts/geoJson/schoolDistricts.geojson"%(env["PROJECT_PATH"], argv[1]),"w") as districtFileOut:
+    json.dump(data, districtFileOut)
+
+
+
+
+# python scripts/mapping/schoolDistricts/generateids.py k12-segregation
--- scripts/mapping/schoolDistricts/shp_to_mbtiles.sh
+++ scripts/mapping/schoolDistricts/shp_to_mbtiles.sh
@@ -1,14 +1,14 @@
 
 # Reproject source shapefile (from NCES) to web mercator
 # Source: https://nces.ed.gov/programs/edge/Geographic/DistrictBoundaries, 2018 boundaries (2017-18 school year)
-ogr2ogr schoolDistricts/shapefile/schoolDistricts.shp -t_srs "EPSG:4326" schoolDistricts/source/EDGE_SCHOOLDISTRICT_TL18_SY1718/schooldistrict_sy1718_tl18.shp
+ogr2ogr data/mapping/schoolDistricts/shapefile/schoolDistricts.shp -t_srs "EPSG:4326" schoolDistricts/source/EDGE_SCHOOLDISTRICT_TL18_SY1718/schooldistrict_sy1718_tl18.shp
 
 #Convert shapefile to geojson
-ogr2ogr -f GeoJSON schoolDistricts/geoJson/schoolDistricts.geojson schoolDistricts/shapefile/schoolDistricts.shp
+ogr2ogr -f GeoJSON data/mapping/schoolDistricts/geoJson/schoolDistricts_tmp.geojson schoolDistricts/shapefile/schoolDistricts.shp
 
 
-##alice add ndjson stuff here
+python scripts/mapping/schoolDistricts/generateids.py k12-segregation
 
 
 #Convert geojson to mbtiles, min zoom level 6 (max 14 by default). Replace old mbtiles
-tippecanoe -o schoolDistricts/mbtiles/schoolDistricts.mbtiles schoolDistricts/geoJson/schoolDistricts.geojson -Z6 --force
+tippecanoe -o data/mapping/schoolDistricts/mbtiles/schoolDistricts.mbtiles schoolDistricts/geoJson/schoolDistricts.geojson -Z6 --force
